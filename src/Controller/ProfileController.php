db21b26cb839c2122cb036027f60567d400c037e
Kasia <kata.drzazgowska@gmail.com>
2023-04-03 21:29:14 +0200

    03042023

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\UserProfile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+// getUserProfile -->
+use App\Repository\UserRepository;   
+use App\Entity\User;
+
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function profile(Request $request): Response
     {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+
+        // ???? ---> 
+       /**  @var User $user */ 
+        $user = $this->getUser();
+        // if null create new
+        $userProfile = $user->getUserProfile() ?? new UserProfile();
+
+
+        $form = $this->createForm(
+            UserProfileType::class, $userProfile
+        );
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $userProfile = $form->getData();
+            //save -> flash -> redirect
+        }
+        return $this->render(
+            'profile/profile.html.twig', [
+            'forl' => $form->createView(),
         ]);
     }
 }
